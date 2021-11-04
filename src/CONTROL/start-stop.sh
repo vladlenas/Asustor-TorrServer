a694651847b2ae39e87d44da70acea9adfd9a37e
vladlenas <54670862+vladlenas@users.noreply.github.com>
2021-11-04 10:03:55 +0200

    Update start-stop.sh

--- src/CONTROL/start-stop.sh
+++ src/CONTROL/start-stop.sh
@@ -2,26 +2,50 @@
 
 
 PKG_PATH=$APKG_PKG_DIR/bin
-PORT=8090
+PID_FILE=/var/run/torrserver.pid
+DAEMON=$APKG_PKG_DIR/bin/torrserver
+
+service_start() {
+  start-stop-daemon --start --quiet --background --make-pidfile --pidfile $PID_FILE --exec $DAEMON -d $PKG_PATH/
+}
+
+service_stop() {
+  start-stop-daemon --stop --pidfile $PID_FILE --exec $DAEMON
+  RETVAL=$?
+  [ $RETVAL -eq 0 ] && [ -e "$PID_FILE" ] && rm -f $PID_FILE
+}
+
+service_status() {
+  start-stop-daemon --status -q -p $PID_FILE
+  if [ -e $PID_FILE ]; then
+     echo TorrServer running
+  else
+     echo TorrServer stopped
+  fi
+}
+
+service_restart() {
+  service_stop
+  service_start
+}
 
 case $1 in
-
   start)
-    /usr/bin/nohup $PKG_PATH/torrserver -p $PORT -d $PKG_PATH &
-    sleep 1
+    service_start
     ;;
-
   stop)
-    /usr/bin/killall torrserver
-    /bin/sync
-    sleep 1
+    service_stop
     ;;
-
+  status|enabled)
+    service_status
+    ;;
+  restart)
+    service_restart
+    ;;
   *)
-    echo "usage: $0 {start|stop}"
+    echo "Usage: $0 {start|stop|status|restart}"
     exit 1
     ;;
-
 esac
 
 exit 0
