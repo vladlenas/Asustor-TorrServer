3931878367ab528c38e84947c9e67af074497438
vladlenas <54670862+vladlenas@users.noreply.github.com>
2023-10-12 20:26:18 +0300

    Update start-stop.sh

--- src/CONTROL/start-stop.sh
+++ src/CONTROL/start-stop.sh
@@ -1,26 +1,38 @@
 #!/bin/sh
 
+if [[ -z $APKG_PKG_DIR ]]; then
+	PKG_DIR=/usr/local/AppCentral/torrserver
+else
+	PKG_DIR="${APKG_PKG_DIR}"
+fi
 
-PKG_PATH=$APKG_PKG_DIR/bin
+SERVICE_COMAND=$PKG_DIR/bin/torrserver
+CONFIG_DIR=$PKG_DIR/config
+LOG_FILE=$PKG_DIR/torrserver.log
 PORT=8090
 
 case $1 in
 
-  start)
-    /usr/bin/nohup $PKG_PATH/torrserver -p $PORT -d $PKG_PATH &
-    sleep 1
-    ;;
+    start)
+      if [[ -r $CONFIG_DIR/accs.db ]]; then   
+          /usr/bin/nohup $SERVICE_COMAND -p $PORT -d $CONFIG_DIR -a -l $LOG_FILE &
+          sleep 1
+      else
+          /usr/bin/nohup $SERVICE_COMAND -p $PORT -d $CONFIG_DIR -l $LOG_FILE &
+          sleep 1
+      fi
+      ;;
 
-  stop)
-    /usr/bin/killall torrserver
-    /bin/sync
-    sleep 1
-    ;;
+    stop)
+      /usr/bin/killall torrserver
+      /bin/sync
+      sleep 1
+      ;;
 
-  *)
-    echo "usage: $0 {start|stop}"
-    exit 1
-    ;;
+    *)
+      echo "usage: $0 {start|stop}"
+      exit 1
+      ;;
 
 esac
 
