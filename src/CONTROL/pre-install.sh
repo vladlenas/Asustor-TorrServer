544025827ad9a5fb706f14289f7fc3e6a98dd70c
vladlenas <54670862+vladlenas@users.noreply.github.com>
2023-10-12 20:24:40 +0300

    Update pre-install.sh

--- src/CONTROL/pre-install.sh
+++ src/CONTROL/pre-install.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
-PKG_PATH=$APKG_PKG_DIR/bin
+if [[ -z $APKG_PKG_DIR ]]; then
+	PKG_DIR=/usr/local/AppCentral/torrserver
+else
+	PKG_DIR="${APKG_PKG_DIR}"
+fi
+
+OLD_CONFIG=$PKG_DIR/bin/config.db
+CONFIG_DIR=$PKG_DIR/config
 
 case "${APKG_PKG_STATUS}" in
 
-	install)
-		;;
-	upgrade)
-	          cp -af $PKG_PATH/config.db $APKG_TEMP_DIR/.
-		;;
-	*)
-		;;
+    install)
+	;;
+    upgrade)   
+	if [[ -f $OLD_CONFIG ]]; then
+	    cp -af $OLD_CONFIG $APKG_TEMP_DIR/
+	else
+	    cp -af $CONFIG_DIR/* $APKG_TEMP_DIR/
+	fi	   
+	;;
+    *)
+	;;
 esac
 
 exit 0
